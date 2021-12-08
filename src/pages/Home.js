bcaff4669d61214e2e4d1cdba436f6d209dd4ae2
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-12-08 09:25:10 -0500

    Completing manual styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import './Home_Resources/Main.css';
-import './Home_Resources/Helper.css';
-import './Global_Resources/jquery-ui.css';
 import Swal from 'sweetalert2';
+
+import zero from './Global_Resources/number/0.png';
+import one from './Global_Resources/number/1.png';
+import two from './Global_Resources/number/2.png';
+import six from './Global_Resources/number/6.png';
 
 export default class Home extends React.Component {
     render() {
@@ -15,7 +18,7 @@ export default class Home extends React.Component {
             <div id="a-home">
                 <div id="searchbox" className="mascotbox">
                     <h1 style={{fontSize: "4em"}}>
-                        <a href="" onClick={(e)=>{e.preventDefault(); Swal.fire({title:"Bruh",text:"This is a yiff site, to keep this project SFW, I wont redirect you", icon: "warning",confirmButtonText:"Fine Whatever"}); }}>e621</a>
+                        <a href="/" onClick={(e)=>{e.preventDefault(); Swal.fire({title:"Bruh",text:"This is a yiff site, to keep this project SFW, I wont redirect you", icon: "warning",confirmButtonText:"Fine Whatever"}); }}>e621</a>
                     </h1>
                     <div id="links">
                         <a title="Login or sign up" href="https://e621.net/session/new">Login/Signup</a>
@@ -30,11 +33,30 @@ export default class Home extends React.Component {
                         <form action="https://e621.net/posts" acceptCharset="UTF-8" method="get">
                             <div>
                                 <input type="text" name="tags" id="tags" defaultValue="" size="30" autoFocus="autofocus" data-autocomplete="tag-query" className="ui-autocomplete-input" autoComplete="off"/><br/>
-                                <input type="submit" value="Search"/>
+                                <input type="submit" value="Search" style={{margin:"5px"}}/>
                                 <input type="button" value="Change Mascot" id="change-mascot"/>
                             </div>
                         </form>
                     </div>
+                    <div id="mascot_artist">Mascot by <a href="http://www.furaffinity.net/user/keishinkae">Keishinkae</a></div>
+                </div>
+                <div id="searchbox2" class="mascotbox">
+                    <img src={two}/>
+                    <img src={six}/>
+                    <img src={one}/>
+                    <img src={zero}/>
+                    <img src={two}/>
+                    <img src={zero}/>
+                    <img src={one}/>
+                </div>
+                <div id="searchbox3" class="mascotbox">
+                    <p>
+                        Serving 2,610,201 posts<br/>
+                        <a title="Takedown Information" href="https://e621.net/static/takedown">Takedown Policy and Process</a>
+                        |
+                        <a title="Contact Us" href="https://e621.net/static/contact">Contact Us</a> |
+                        <a title="Terms of Service" href="https://e621.net/static/terms_of_service">Terms of Service</a>
+                    </p>
                 </div>
             </div>
         </React.Fragment>
