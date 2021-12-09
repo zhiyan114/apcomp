5d3d54ef1ed39a3fb23030ca3c66ad58f6e28625
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-12-09 13:12:20 -0500

    Completed search api

--- server.py
+++ server.py
@@ -24,8 +24,27 @@ def staticcss_file(path):
 @app.route('/api/search', methods=['POST'])
 def api_test():
     req_data = flask.request
-    google_res = requests.get(f"https://www.googleapis.com/customsearch/v1?key={os.getenv('GOOGLE_API','')}&q={req_data.json['data']}&cx={os.getenv('GOOGLE_CX','')}")
-    response = flask.make_response(google_res.content.decode("utf-8"), 200)
+    google_res = requests.get(f"https://www.googleapis.com/customsearch/v1?key={os.getenv('GOOGLE_API','')}&q={req_data.json['data']}&cx={os.getenv('GOOGLE_CX','')}&num=4&searchType=image&start=1")
+    tmp_data = {}
+    tmp_data['data'] = []
+    for i in json.loads(google_res.content.decode("utf-8"))['items']:
+        tmp_data.data.append({
+            "title": i['title'],
+            "imgurl": i['link'],
+            "contexturl": i['image']['contextLink'],
+            "thumbnail": {
+                "url": i['image']['thumbnailLink'],
+                "width": i['image']['thumbnailWidth'],
+                "height": i['image']['thumbnailHeight']
+            }
+        })
+    if(len(search_history) > 10):
+        search_history.pop(len(search_history)-1)
+        search_history.insert(0,req_data.json['data'])
+    else:
+        search_history.append(req_data.json['data'])
+    tmp_data.history = search_history
+    response = flask.make_response(json.dumps(tmp_data), 200)
     response.mimetype = 'application/json'
     return response
 @app.route('/api/search/history', methods=['GET'])
