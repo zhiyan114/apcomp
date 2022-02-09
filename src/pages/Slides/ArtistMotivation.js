bbd73d16a84bbde9df59d679bf289b065fb08f92
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-02-09 09:39:52 -0500

    Improvised missing information

--- src/pages/Slides/ArtistMotivation.js
+++ src/pages/Slides/ArtistMotivation.js
@@ -91,7 +91,12 @@ export default class Main extends React.Component {
                     <Sidebar SearchVal={this.state.Keyword} HistoryItems={this.state.SearchHistory}/>
                     <section className="content">
                         <div style={{textAlign:"center"}}>
-                            <h1 style={{fontSize:"2rem",marginBottom:"1.5rem"}}>Artist's Motivation</h1>
+                            <h1 style={{fontSize:"2rem",marginBottom:"1.5rem"}}>Reflection</h1>
+                            <p>
+                            It had a religional meaning, about death.
+In the subsequent verses, the narrator uses various parts of a typical building including window panes, hinges and floor as metaphors to explain how this “house” is overly faulty and no longer functional. The narrator concludes by emphasizing the fact that this body is no more useful. He mentions meeting the saints to depict how people believe that the soul and spirit of a person leaves the mortal body to go to heaven when a person dies.
+                            </p>
+                            <h1 style={{fontSize:"2rem",margin:"1.5rem"}}>Artist's Motivation</h1>
                             <p>
                             Stuart tells people “The story: A friend of mine and myself were hunting up in the High Sierras. We found a little old cabin that had almost been demolished by a wild storm. The only living thing around it was a starving old hound dog. In the back room we found a little old prospector that had cashed in his blue chips.”
 "Later on, riding down the canyon, with the old hound dog on the pommel of my saddle, taking him into shelter and to food, I got to thinking."This old house, the ones made of wood and steel, shall all come down. And this old house of mine, made of clay, it's got to go too - but there's a big difference. Although the wooden house, the steel house, or brick house may be scattered in the winds of a wild storm and its debris scattered over the hillside, this clay house in which I live shall be scattered too. But . . . the soul inside shall, in God's own good time, be gathered with the saints.
